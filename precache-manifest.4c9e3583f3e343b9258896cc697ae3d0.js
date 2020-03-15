73772575a01141f28c259e6b9556b2a629d277dc
Serbin9 <serbin_y@ukr.net>
2020-03-15 21:08:06 +0200

    Updates

---
+++ precache-manifest.4c9e3583f3e343b9258896cc697ae3d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0730cb7311a575aec24c6100a5ae6ea1",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "3ceca92d5d05ad5100f8",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.6a78645b.chunk.css"
+  },
+  {
+    "revision": "c41156e8a24465a77dcd",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.faebf747.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.faebf747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ceca92d5d05ad5100f8",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.070e4ab1.chunk.js"
+  },
+  {
+    "revision": "7ca7435ee77538f77453",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.f8f0acc9.js"
+  }
+]);
